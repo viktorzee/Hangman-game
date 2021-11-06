28ce243ed2d349a8ee1f936fa5d9f7260c0d6c4a
Victor Opara <opara.victor@gmail.com>
2021-11-06 03:32:47 +0100

    styling and basic functionality

---
+++ scripts/request.js
@@ -0,0 +1,14 @@
+const getPuzzle = async (wordCount) => {
+    try {
+        const response = await fetch(`//puzzle.mead.io/puzzle?wordCount=${wordCount}`);
+        if (response.status === 200) {
+
+         const data = await response.json();
+         return data.puzzle;
+        } else {
+            throw new Error('unable to fetch puzzle');
+        }
+    } catch (err) {
+        console.log(`Error: ${err}`);
+    }
+}
